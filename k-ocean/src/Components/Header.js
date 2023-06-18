61b22a6e7702b05b867e3cde7c4b6cea62bb4f06
Tyler Cahoon-Kasheta <tyler.kasheta@outlook.com>
2023-06-18 19:25:36 -0400

    adds navbar with functional routes

--- k-ocean/src/Components/Header.js
+++ k-ocean/src/Components/Header.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-const Header = () => {
+function Example(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div>
-      <h1>Kody Ocean Landscape Photography</h1>
+      <Navbar
+      className="my-3"
+      color="info"{...args}>
+        <NavbarBrand href="/">
+         <img
+          alt="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSh_gfvqE5bcRBBg4GYcPgbLMSOTH8cSarzmA&usqp=CAU"
+          style={{
+            height: 50,
+            width: 50
+          }}
+          /> Kody Ocean Landscapes
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/gallery/">
+                Gallery
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="aboutMe">
+                About Me
+              </NavLink>
+            </NavItem> 
+            <NavItem>
+              <NavLink href="contact">
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>   
+        </Collapse>
+      </Navbar>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Example;
